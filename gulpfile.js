8b2f8e611f788679a2cc6c2260849a7c80e1f390
Aaron Ofengender <aofengen@gmail.com>
2017-09-07 12:05:48 -0400

    initial commit for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es').default;
+const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
+const notify = require('gulp-notify');
 
 const javascriptFiles = [
 	'./app.js',
@@ -15,9 +17,20 @@ gulp.task('bundle', function() {
 	return gulp.src(javascriptFiles)
 	.pipe(sourcemaps.init())
 	.pipe(concat('bundle.js')) //squish files together into one file
+	// .pipe(gulp.dest("./dist"))
+	// .pipe(plumber({
+	// 	errorHandler: notify.onError({
+	// 		title: 'Gulp',
+	// 		message: '<%= error.message %>',
+	// 	})
+	// }))
 	.pipe(uglify())
 	.pipe(sourcemaps.write('./maps/'))
-	.pipe(gulp.dest("./dist")); //save bundle.js  								 	  
+	.pipe(gulp.dest("./dist")); //save bundle.js  
+	// .pipe(notify({
+	// 	title: 'Gulp',
+	// 	message: 'Scripts Done'
+	// })); 								 	  
 });
 
 gulp.task('watch', function(){
