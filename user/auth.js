cbf5ccf12fdcf8ea7ae89fd7ad67c35564dfc785
Aaron Ofengender <aofengen@gmail.com>
2017-09-11 14:19:35 -0400

    deleting changes that broke application

--- user/auth.js
+++ user/auth.js
@@ -2,51 +2,47 @@ $(function(){
 	$.extend(WorkoutLog, {
 		//signup method
 		signup: function() {
-			//username and password variables
-			let username = $("#su_username").val();
-			let password = $("#su_password").val();
-			if (username === "" || password === "") {
-				$("#su_error").text("Please enter a username and password").show();
-			} else {
-				//user object
-				let user = {
-					user: {
-						username: username,
-						password: password
-					}
+		//username and password variables
+		let username = $("#su_username").val();
+		let password = $("#su_password").val();
+			//user object
+			let user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			//signup post
+			let signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			});
+
+			//signup done/fail
+			signup.done(function(data){
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+					WorkoutLog.definition.fetchAll();
+					WorkoutLog.log.fetchAll();
 				};
-
-				//signup post
-				let signup = $.ajax({
-					type: "POST",
-					url: WorkoutLog.API_BASE + "user",
-					data: JSON.stringify(user),
-					contentType: "application/json"
-				});
-
-				//signup done/fail
-				signup.done(function(data){
-					if (data.sessionToken) {
-						WorkoutLog.setAuthHeader(data.sessionToken);
-						WorkoutLog.definition.fetchAll();
-						WorkoutLog.log.fetchAll();
-					};
-					$("#signup-modal").modal("hide");
-					$(".disabled").removeClass("disabled");
-					$(".hidden").removeClass("hidden");
-					// $("#homeTab").hide().addClass("disabled");
-					$(".tab1").show();
-					$("#logoutTab").text("Logout");
-					$("#defineTab").text("Define");
-					$("#logTab").text("Log");
-					$("#historyTab").text("View History");
-					$("#su_username").val("");
-					$("#su_password").val("");
-					$('a[href="#define"]').tab("show"); //routing
-				}).fail(function() {
-					$("#su_error").text("There was an issue with sign up").show();
-				})
-			} 
+				$("#signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$(".hidden").removeClass("hidden");
+				// $("#homeTab").hide().addClass("disabled");
+				$(".tab1").show();
+				$("#logoutTab").text("Logout");
+				$("#defineTab").text("Define");
+				$("#logTab").text("Log");
+				$("#historyTab").text("View History");
+				$("#su_username").val("");
+				$("#su_password").val("");
+				$('a[href="#define"]').tab("show"); //routing
+			}).fail(function() {
+				$("#su_error").text("There was an issue with sign up").show();
+			}) 
 		},
 
 		//login method
@@ -54,46 +50,42 @@ $(function(){
 			//login variables
 			let username = $("#li_username").val();
 			let password = $("#li_password").val();
-			if (username === "" || password === "") {
-				$("#li_error").text("Please enter a username and password").show();
-			} else {
-				let user = {
-					user: {
-						username: username,
-						password: password
-					}
-				};
+			let user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
 
-				//login POST
-				let login = $.ajax({
-					type: "POST",
-					url: WorkoutLog.API_BASE + "login",
-					data: JSON.stringify(user),
-					contentType: "application/json"
-				})
-				//login done/fail
-				login.done(function(data) {
-					if (data.sessionToken) {
-						WorkoutLog.setAuthHeader(data.sessionToken);
-						WorkoutLog.definition.fetchAll();
-						WorkoutLog.log.fetchAll();
-					}
-					$("#login-modal").modal("hide");
-					$(".disabled").removeClass("disabled");
-					$(".hidden").removeClass("hidden");
-					// $("#homeTab").hide().addClass("disabled");
-					$(".tab1").show();
-					$("#logoutTab").text("Logout");
-					$("#defineTab").text("Define");
-					$("#logTab").text("Log");
-					$("#historyTab").text("View History");
-					$("#li_username").val("");
-					$("#li_password").val("");
-					$('a[href="#define"]').tab("show");
-				}).fail(function (){
-					$("#li_error").text("Unable to login.").show();
-				});
-			} 
+			//login POST
+			let login = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "login",
+				data: JSON.stringify(user),
+				contentType: "application/json"
+			})
+			//login done/fail
+			login.done(function(data) {
+				if (data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+					WorkoutLog.definition.fetchAll();
+					WorkoutLog.log.fetchAll();
+				}
+				$("#login-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$(".hidden").removeClass("hidden");
+				// $("#homeTab").hide().addClass("disabled");
+				$(".tab1").show();
+				$("#logoutTab").text("Logout");
+				$("#defineTab").text("Define");
+				$("#logTab").text("Log");
+				$("#historyTab").text("View History");
+				$("#li_username").val("");
+				$("#li_password").val("");
+				$('a[href="#define"]').tab("show");
+			}).fail(function (){
+				$("#li_error").text("Unable to login.").show();
+			}); 
 		},
 		//loginoutmethod
 		logout: function() {
