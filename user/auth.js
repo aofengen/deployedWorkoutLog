262ff0c357e6defb57470c7304900d8e6edb92d1
Aaron Ofengender <aofengen@gmail.com>
2017-09-11 13:59:11 -0400

    bugfixes

--- user/auth.js
+++ user/auth.js
@@ -5,7 +5,7 @@ $(function(){
 			//username and password variables
 			let username = $("#su_username").val();
 			let password = $("#su_password").val();
-			if (username === "" || password === "") {
+			if (username !== "" || password !== "") {
 				//user object
 				let user = {
 					user: {
@@ -54,7 +54,7 @@ $(function(){
 			//login variables
 			let username = $("#li_username").val();
 			let password = $("#li_password").val();
-			if (username ==="" || password === "") {
+			if (username !== "" || password !== "") {
 				let user = {
 					user: {
 						username: username,
